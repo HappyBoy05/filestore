44c8310c948d74a6ec92cef51e1d776363524379
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-29 21:15:29 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -26,12 +26,8 @@ async def storefile(c, m):
                 text += f"🎵 __Title:__ `{media.title}`\n\n" if media.title else ""
                 text += f"🎙 __Performer:__ `{media.performer}`\n\n" if media.performer else ""
     text += f"__✏ Caption:__ `{m.caption}`\n\n" if m.caption else ""
-    text += "**--Uploader Details:--**\n\n\n"
-    text += f"__🦚 First Name:__ `{m.from_user.first_name}`\n\n"
-    text += f"__🐧 Last Name:__ `{m.from_user.last_name}`\n\n" if m.from_user.last_name else ""
-    text += f"__👁 User Name:__ @{m.from_user.username}\n\n" if m.from_user.username else ""
-    text += f"__👤 User Id:__ `{m.from_user.id}`\n\n"
-    text += f"__💬 DC ID:__ {m.from_user.dc_id}\n\n" if m.from_user.dc_id else ""
+    text += f"Link - `url`"
+
 
     # if databacase channel exist forwarding message to channel
     if DB_CHANNEL_ID:
