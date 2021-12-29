779add98f17ab3abaf6d795486c30ebb3d890a6f
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-29 19:35:24 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -41,9 +41,9 @@ async def storefile(c, m):
     # creating urls
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
-    url = f"https://t.me/{bot.username}?start={base64_string}"
+    url = f"https://t.me/Tgfilestorerobot?start={base64_string}"
     txt = urllib.parse.quote(text.replace('--', ''))
-    share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
+    share_url = f"tg://share?url={url}"
 
     # making buttons
     buttons = [[
@@ -96,9 +96,9 @@ async def storefile_channel(c, m):
     # creating urls
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
-    url = f"https://t.me/{bot.username}?start={base64_string}"
+    url = f"https://t.me/Tgfilestorerobot?start={base64_string}"
     txt = urllib.parse.quote(text.replace('--', ''))
-    share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
+    share_url = f"tg://share?url={url}"
 
     # making buttons
     buttons = [[
